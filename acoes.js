9732ab2a27e8726778785bbfa8926317149ec76d
Matheus Mattos <matheussilvamattos202@gmail.com>
2024-06-04 18:49:59 -0300

    .

---
+++ acoes.js
@@ -0,0 +1,19 @@
+function acao1(){
+let resp = window.document.getElementById(`saida`)
+resp.innerHTML += `<p>Clicou no botão 1<p>`
+}
+
+    function acao2(){
+    let resp = window.document.getElementById(`saida`)
+    resp.innerHTML += `<p>Clicou no botão 2<p>`
+    }
+
+        function acao3(){
+        let resp = window.document.getElementById(`saida`)
+        resp.innerHTML += `<p>Clicou no botão 3<p>`
+        }
+
+            function acao4(){
+            let resp = window.document.getElementById(`saida`)
+            resp.innerHTML += `<p>Clicou no botão 4<p>`
+            }
